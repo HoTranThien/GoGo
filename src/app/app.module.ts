36b8dc5c5fd4f510f5c744fa982d5221474ed581
Ho Tran <htran3@strongtie.com>
2024-02-16 23:53:50 +0700

    test 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SideViewComponent } from './side-view/side-view.component';
+import { MainBodyComponent } from './main-body/main-body.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      HeaderComponent,
+      FooterComponent,
+      SideViewComponent,
+      MainBodyComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
